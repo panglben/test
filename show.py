b4f4ce0ad0465c7e0916f6acbb8ec44dbfc8f293
yl <yinglei0416@163.com>
2022-01-09 23:01:33 +0800

    try to use canvas

--- show.py
+++ show.py
@@ -2,20 +2,28 @@ import numpy as np
 import tkinter as tk
 import tkinter.ttk as ttk
 
-win = tk.Tk()
-win.size = np.array([450,450])
+class GUIWindow:
+    def __init__(self, windowname):
+        self.win = tk.Tk()
+        self.win.title(windowname)
+        self.win.size = np.array([800,800])
 
-canvas = tk.Canvas(win, width=win.size[0], height=win.size[1], highlightthickness=0)
+        self.canvas = tk.Canvas(self.win, bg='white')
 
-def circle(self, color, selected, *bbox):
-    if selected:
-        outline = '#004500'
-        width = 3
-    else:
-        outline = 'black'
-        width = 1
-    self.canvas.create_oval(*tuple(int(x) for x in bbox), fill=color,
-                            outline=outline, width=width)
+    def circle(self, color, selected, *bbox):
+        if selected:
+            outline = '#004500'
+            width = 3
+        else:
+            outline = 'black'
+            width = 1
+        self.canvas.create_oval(*tuple(int(x) for x in bbox), fill=color,
+                                outline=outline, width=width)
 
-from ase.cli.main import old
-old()
+
+gui = GUIWindow('test')
+gui.circle("#ffffff", True, 10, 20, 10, 20)
+
+gui.canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+gui.win.mainloop()
