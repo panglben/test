68a8b0f09ae59771fccc8218343fd527a15b5c72
yl <yinglei0416@163.com>
2022-01-23 19:55:32 +0800

    try to use class

--- show.py
+++ show.py
@@ -9,6 +9,8 @@ class GUIWindow:
         self.win.size = np.array([800,800])
 
         self.canvas = tk.Canvas(self.win, bg='white')
+        
+        self.canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
     def circle(self, color, selected, *bbox):
         if selected:
@@ -22,8 +24,11 @@ class GUIWindow:
 
 
 gui = GUIWindow('test')
-gui.circle("#ffffff", True, 10, 20, 10, 20)
+r = 20
+A1 = [120, 120]
+A2 = [125, 125]
+gui.circle("#ffffff", False, A2[0], A2[1], A2[0]+r, A2[1]+r)
+gui.circle("#ffffff", False, A1[0], A1[1], A1[0]+r, A1[1]+r)
 
-gui.canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
 gui.win.mainloop()
